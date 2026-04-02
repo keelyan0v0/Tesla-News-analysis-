94444fab349a51e68242b5ad961cda4967291544
keelyan0v0 <keelyan.0v0@gmail.com>
2026-04-03 00:35:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -381,10 +381,12 @@ with side_placeholder:
     for s in signals
 ]
 
-selected_option = st.selectbox("Select News Signal", options)
+selected_option = st.selectbox(
+    "Select News Signal",
+    options
+)
 
-selected_signal = signals[options.index(selected_option)
-]
+selected_signal = signals[options.index(selected_option)]
 
         selected_signal = next(
             s for s in signals if s["title"] == selected_title
